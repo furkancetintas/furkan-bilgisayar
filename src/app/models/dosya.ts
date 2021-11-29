e90d01c488f9a89b83bf2474e4129ff5a8f2bddd
Furkan Çetintaş <66469657+furkancetintas@users.noreply.github.com>
2021-11-30 02:25:23 +0300

    Update dosya.ts

--- src/app/models/dosya.ts
+++ src/app/models/dosya.ts
@@ -15,3 +15,21 @@ export class Dosya {
     kayTarih: string;
     duzTarih: string;
 }
+
+export class Dosya {
+    key: string;
+    adi: string;
+    uid: string;
+    url: string;
+    file: File;
+    urunAd: string;
+    urunBilgi: string;
+    urunKategori: string;
+    urunFiyat: string;
+    urunRam: string;
+    urunIslemciHizi: string;
+    urunDiskKapasitesi: string;
+    urunEkranKartiHafizasi: string;
+    kayTarih: string;
+    duzTarih: string;
+}
